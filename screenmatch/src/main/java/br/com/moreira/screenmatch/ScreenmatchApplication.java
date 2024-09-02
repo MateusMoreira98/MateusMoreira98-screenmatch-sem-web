731824f9d9d3598333004e1d3940475546a1ea24
mateusmoreira <mateusmoreira090@hotmail.com>
2024-09-02 12:36:45 -0300

    Criação da Classe Record DadosEpisodio

--- screenmatch/src/main/java/br/com/moreira/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/moreira/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package br.com.moreira.screenmatch;
 
+import br.com.moreira.screenmatch.model.DadosEpisodio;
 import br.com.moreira.screenmatch.model.DadosSerie;
 import br.com.moreira.screenmatch.service.ConsumoAPI;
 import br.com.moreira.screenmatch.service.ConverteDados;
@@ -17,11 +18,16 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 		var consumoApi = new ConsumoAPI();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=4f5be9c3");
+		var json = consumoApi.obterHTTP("https://www.omdbapi.com/?t=gilmore+girls&apikey=4f5be9c3");
 		System.out.println(json);
 
 		ConverteDados conversor = new ConverteDados();
+
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+
+		json = consumoApi.obterHTTP("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=4f5be9c3");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
 	}
 }
